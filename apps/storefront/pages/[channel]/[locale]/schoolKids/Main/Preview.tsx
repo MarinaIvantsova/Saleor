25fe346131509fb9c8c0a7081362f6a7f6c0e09f
Marina Ivantsova <marineivantsova@yandex.ru>
2023-07-05 21:18:26 +0700

    Changed the h2 width

--- apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Preview.tsx
+++ apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Preview.tsx
@@ -4,7 +4,7 @@ function Preview() {
       <h1 className="visually-hidden">Школа английского языка для детей Clever Baby</h1>
       <div className="flex relative min-h-[512px] items-center 2xl:bg-main-photo 2xl:w-min-w-full 2xl:bg-no-repeat 2xl:bg-100% md:bg-main-photo-tablet md:bg-100% md:bg-no-repeat xs:bg-main-photo-mobile xs:bg-no-repeat xs:bg-100% xs:min-h-[304px]">
         <section className="relative w-[70%] 2xl:before:absolute 2xl:before:content-carIcon 2xl:before:top-[7%] 2xl:before:-left-[15%] 2xl:before:w-[106px] 2xl:before:h-[84px] 2xl:after:absolute 2xl:after:top-[8%] 2xl:after:left-[103%] 2xl:after:content-cloudIcon 2xl:mx-auto 2xl:pt-[136px] 2xl:pb-[347px] 2xl:after:h-[62px] 2xl:after:w-[91px] md:before:absolute md:before:-top-[45%] md:before:-left-[5%] md:before:content-carIcon md:before:w-[58px] md:before:h-[46px] md:mx-auto md:w-[90%] xs:before:absolute xs:before:-top-[15%] xs:before:-left-[20%] xs:before:content-carIcon xs:before:w-[48px] xs:before:h-[38px] xs:mx-auto xs-w-[85%]">
-          <h2 className="mt-0 mb-[2px] font-rubik 2xl:text-[42px] 2xl:leading-[45px] w-[39%] min-w-[380px] text-layoutTextColor md:text-[42px] xs:text-[28px]">
+          <h2 className="mt-0 mb-[2px] font-rubik 2xl:text-[42px] 2xl:leading-[45px] w-[40%] min-w-[380px] text-layoutTextColor md:text-[42px] xs:text-[28px]">
             Английский язык
           </h2>
           <p className="mt-0 font-rubik font-medium text-[18px] leading-25 w-[40%] min-w-[380px] text-layoutTextColor xs:text-md">
