f62e75ab2d803519d48536e65e70010e48300915
Marina Ivantsova <marineivantsova@yandex.ru>
2023-07-11 14:37:31 +0700

    Preview and process after code review

--- apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Preview.tsx
+++ apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Preview.tsx
@@ -3,14 +3,14 @@ function Preview() {
     <>
       <h1 className="visually-hidden">Школа английского языка для детей Clever Baby</h1>
       <div className="flex justify-center items-center 2xl:min-h-[512px] 2xl:bg-main-photo 2xl:bg-no-repeat 2xl:bg-100% md:bg-main-photo-tablet md:bg-100% md:bg-no-repeat md:min-h-[441px] xs:bg-main-photo-mobile xs:bg-no-repeat xs:bg-100% xs:min-h-[304px]">
-        <section className="relative 2xl:before:absolute 2xl:w-[70%] 2xl:before:bg-car-icon-bg 2xl:before:top-[7%] 2xl:before:-left-[15%] 2xl:before:w-[106px] 2xl:before:h-[84px] 2xl:after:absolute 2xl:after:top-[8%] 2xl:after:left-[103%] 2xl:after:content-cloudIcon 2xl:mx-auto 2xl:pt-[136px] 2xl:pb-[180px] 2xl:after:h-[62px] 2xl:after:w-[91px] md:before:absolute md:before:-top-[20%] md:before:-left-[4%] md:before:bg-car-icon-bg md:before:w-[58px] md:before:h-[46px] md:before:bg-58px md:mx-auto md:w-[88%] md:before:bg-no-repeat xs:w-[84%] xs:py-[40px] xs:before:absolute xs:before:top-[3%] xs:before:-left-[8%] xs:before:bg-car-icon-bg xs:before:bg-48px xs:before:w-[48px] xs:before:h-[38px] xs:before:bg-no-repeat">
+        <section className="relative 2xl:w-[70%] 2xl:pt-[136px] 2xl:pb-[180px] before:absolute before:bg-car-icon before:top-[10%] before:-left-[15%] 2xl:before:w-[106px] 2xl:before:h-[84px] after:absolute after:top-[10%] after:left-[100%] 2xl:after:content-cloudIcon md:w-[90%] md:before:-top-[20%] md:before:-left-[5%] md:before:bg-58px md:before:w-[58px] md:before:h-[46px] xs:w-[85%] xs:py-[60px] xs:before:top-[4%] xs:before:-left-[5%] xs:before:bg-48px xs:before:w-[48px] xs:before:h-[38px]">
           <h2 className="mt-0 mb-[2px] font-rubik 2xl:text-[42px] 2xl:leading-[45px] text-layoutTextColor md:text-[42px] xs:text-[28px] font-medium">
             Английский язык
           </h2>
           <p className="mt-0 font-rubik font-medium text-[18px] leading-25  text-layoutTextColor xs:text-md">
             для детей от&nbsp;2&nbsp;до&nbsp;7&nbsp;лет
           </p>
-          <p className="2xl:w-[40%] 2xl:min-w-[375px] mt-[30px] mb-[45px] md:w-[40%] md:min-w-[375px] xs:w-[40%] xs:min-w-[224px] text-layoutTextColor xs:text-[14px] font-montserrat text-md leading-[22px]">
+          <p className="2xl:w-[40%] min-w-[375px] mt-[30px] mb-[45px] md:w-[40%] xs:w-[40%] xs:min-w-[225px] text-layoutTextColor xs:text-[14px] font-montserrat text-md leading-[22px]">
             Гармоничное развитие детей и&nbsp;обучение через полное погружение в&nbsp;языковую среду
           </p>
 
--- apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Process.tsx
+++ apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Process.tsx
@@ -1,12 +1,12 @@
 function Process() {
   return (
     <section className="2xl:mt-[118px] md:mt-[98px] xs:mt-[55px]">
-      <div className="flex relative flex-wrap mx-auto 2xl:w-[70%] md:w-[88%] xs:w-[90%] 2xl:before:absolute before:bg-no-repeat 2xl:before:bg-girl-icon-bg 2xl:before:top-[7%] 2xl:before:-left-[15%] 2xl:before:w-[105px] 2xl:before:h-[115px] after:absolute after:bg-rocket-icon-bg after:bg-no-repeat after:-top-[15%] after:left-[85%] 2xl:after:bg-135px 2xl:after:w-[135px] 2xl:after:h-[68px] md:after:bg-101px md:after:w-[101px] md:after:h-[50px] xs:after:-top-[7%] xs:after:left-[80%] xs:after:w-[67px] xs:after:h-[34px] xs:after:bg-67px">
+      <div className="flex relative flex-wrap mx-auto w-[90%] 2xl:w-[70%] before:absolute before:bg-no-repeat 2xl:before:bg-girl-icon before:top-[5%] before:-left-[15%] before:w-[105px] before:h-[115px] after:absolute after:bg-rocket-icon after:bg-no-repeat after:-top-[15%] after:left-[85%] 2xl:after:w-[135px] 2xl:after:h-[68px] md:after:bg-101px md:after:w-[101px] md:after:h-[50px] xs:after:-top-[7%] xs:after:left-[80%] xs:after:w-[67px] xs:after:h-[34px] xs:after:bg-67px">
         <h2 className="2xl:mb-[70px] md:mb-[30px] xs:text-[22px] xs:leading-[30px] xs:mb-[28px] w-full font-rubik text-[36px] text-layoutTextColor leading-[50px] text-center font-medium">
           Как проходит обучение
         </h2>
-        <div className="2xl:w-[40%] md:w-[40%] 2xl:mr-[85px] md:mr-[41px] xs:w-full xs:mb-[15px]">
-          <h3 className="2xl:text-[20px] 2x:leading-[30px] md:w-[70%] md:text-[18px] xs:text-md xs:leading-[22px] xs:mb-[15px] xs:w-[60%] mb-[20px] text-layoutTextColor font-montserrat font-medium">
+        <div className="w-[40%] 2xl:mr-[85px] md:mr-[41px] xs:w-full xs:mb-[15px]">
+          <h3 className="2xl:text-[20px] 2xl:leading-[30px] md:w-[70%] md:text-[18px] xs:text-md xs:leading-[22px] xs:mb-[15px] xs:w-[60%] mb-[20px] text-layoutTextColor font-montserrat font-medium">
             Играя&nbsp;— обучаем, обучая&nbsp;— развиваем
           </h3>
           <div className="2xl:text-md 2xl:leading-[23px] md:text-base md:leading-[20px] font-montserrat text-layoutTextColor xs:text-base">
@@ -16,20 +16,20 @@ function Process() {
               ведь малыши не&nbsp;зубрят материал по учебникам, а&nbsp;познают его через игру,
               сказки, книги и&nbsp;музыку.
             </p>
-            <p className="mb-[12px]">
+            <p>
               Наша методика также включает в&nbsp;себя обучение по&nbsp;программе Cambridge.
               Благодаря этому, наши ученики без труда сдают международные и&nbsp;государственные
               экзамены.
             </p>
           </div>
         </div>
-        <div className="md:w-[50%] md:min-w-[354px] xs:w-full xs:min-w-[290px]">
+        <div className="md:w-[50%] md:min-w-[355px] xs:w-full xs:min-w-[290px]">
           <h3 className="2xl:mb-[20px] 2xl:text-[20px] 2xl:leading-[30px] md:w-[70%] md:mb-[23px] md:text-[18px] xs:w-[80%] xs:mb-[15px] xs:text-md xs:leading-[22px] text-layoutTextColor font-montserrat font-medium">
             Посмотрите, как проходят наши занятия
           </h3>
-          <div className="md:w-[354px] md:h-[270px] xs:w-[290px] xs:h-[262px] xs:mx-auto">
+          <div className="md:w-[355px] md:h-[270px] xs:w-[290px] xs:h-[260px] xs:mx-auto">
             <video
-              className="block w-[431px] h-[231px]  rounded-xl object-cover bg-transparent"
+              className="block w-[430px] h-[230px] rounded-xl object-cover bg-transparent"
               controls
               poster="/baby-video.svg"
             >
