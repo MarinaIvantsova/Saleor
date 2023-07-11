ae652cc421efc59e5d27da73a82711602e8ddf5b
Marina Ivantsova <marineivantsova@yandex.ru>
2023-07-11 18:19:36 +0700

    Deleted margins at Process

--- apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Process.tsx
+++ apps/storefront/pages/[channel]/[locale]/schoolKids/Main/Process.tsx
@@ -5,11 +5,12 @@ function Process() {
         <h2 className="2xl:mb-[70px] md:mb-[30px] xs:text-[22px] xs:leading-[30px] xs:mb-[28px] w-full font-rubik text-[36px] text-layoutTextColor leading-[50px] text-center font-medium">
           Как проходит обучение
         </h2>
-        <div className="w-[40%] 2xl:mr-[85px] md:mr-[41px] xs:w-full xs:mb-[15px]">
-          <h3 className="2xl:text-[20px] 2xl:leading-[30px] md:w-[70%] md:text-[18px] xs:text-md xs:leading-[22px] xs:mb-[15px] xs:w-[60%] mb-[20px] text-layoutTextColor font-montserrat font-medium">
+        {/* 2xl:mr-[85px] md:mr-[41px] xs:w-full xs:mb-[15px] */}
+        <div className="w-[40%] ">
+          <h3 className="2xl:text-[20px] 2xl:leading-[30px] md:w-[70%] md:text-[18px] xs:text-md xs:leading-[22px] xs:mb-[15px] xs:w-min-[175px] mb-[20px] text-layoutTextColor font-montserrat font-medium">
             Играя&nbsp;— обучаем, обучая&nbsp;— развиваем
           </h3>
-          <div className="2xl:text-md 2xl:leading-[23px] md:text-base md:leading-[20px] font-montserrat text-layoutTextColor xs:text-base">
+          <div className="w-[85%] xs:w-[285px] 2xl:text-md 2xl:leading-[23px] md:text-base md:leading-[20px] font-montserrat text-layoutTextColor xs:text-base">
             <p className="mb-[12px]">
               Мы&nbsp;взяли все самое лучшее из традиционных и&nbsp;современных методик обучения
               и&nbsp;развития детей. Обучение английскому языку происходит легко и&nbsp;интересно,
@@ -24,10 +25,10 @@ function Process() {
           </div>
         </div>
         <div className="md:w-[50%] md:min-w-[355px] xs:w-full xs:min-w-[290px]">
-          <h3 className="2xl:mb-[20px] 2xl:text-[20px] 2xl:leading-[30px] md:w-[70%] md:mb-[23px] md:text-[18px] xs:w-[80%] xs:mb-[15px] xs:text-md xs:leading-[22px] text-layoutTextColor font-montserrat font-medium">
+          <h3 className="mb-[20px] 2xl:text-[20px] 2xl:leading-[30px] md:w-[70%] md:text-[18px] xs:w-[80%] xs:mt-[35px] xs:text-md xs:leading-[22px] text-layoutTextColor font-montserrat font-medium">
             Посмотрите, как проходят наши занятия
           </h3>
-          <div className="md:w-[355px] md:h-[270px] xs:w-[290px] xs:h-[260px] xs:mx-auto">
+          <div className="md:w-[355px] md:h-[270px] xs:w-[290px] xs:h-[260px]">
             <video
               className="block w-[430px] h-[230px] rounded-xl object-cover bg-transparent"
               controls
