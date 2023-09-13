9a293d14dee385b9395b1649e15137a099352dbb
Marina Ivantsova <marineivantsova@yandex.ru>
2023-09-13 11:35:02 +0700

    Deleted comments

--- apps/storefront/components/AppContainer/styles.tsx
+++ apps/storefront/components/AppContainer/styles.tsx
@@ -1,14 +1,1 @@
-// import { makeStyles } from "@saleor/macaw-ui";
-
-// export const useStyles = makeStyles(
-//   (theme) => ({
-//     root: {
-//       padding: theme.spacing(0, 0, 14, 0),
-//     },
-//   }),
-//   {
-//     name: "AppContainer",
-//   }
-// );
-
 export {};
